49a5121be0028c07ff2a71e43f7a3b4386692de3
MASHA <mashanikit@gmail.com>
2017-07-18 11:49:11 -0400

    character 1 Ned is done

--- js/scene2.js
+++ js/scene2.js
@@ -5,10 +5,36 @@ function scene2() {
   stage = document.querySelector('#stage')
   castle = document.querySelector('#castle')
   castleImgSrc = castle.getAttribute('src')
-    stageImg.setAttribute('src', castleImgSrc)
   stageImg = stage.querySelector('#stage-img')
+  stageImg.setAttribute('src', castleImgSrc)
+
 
   // Setup Cast
+  nedContainer = document.createElement('div')
+    stage.append(nedContainer)
+
+    nedContainer.style.position = 'absolute'
+    nedContainer.style.width = '10%'
+    nedContainer.style.height = '37%'
+    ned.style.width = '70%'
+    nedContainer.style.top = '55%'
+    nedContainer.style.left = '25%'
+    nedContainer.style.border = 'none'
+    nedContainer.style.background = 'none'
+
+    ned = document.querySelector('#ned')
+    nedContainer.append(ned)
+
+    leather = document.querySelector('#leather-armour')
+    nedContainer.append(leather)
+
+  ned.style.border = 'none'
+  leather.style.border = 'none'
+
+  ned.style.position = 'absolute'
+  ned.style.top = 0
+  leather.style.position = 'absolute'
+  leather.style.bottom = 0
 
   // Character 1
 
